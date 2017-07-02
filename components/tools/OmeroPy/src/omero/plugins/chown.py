a0bfb68719c2ec0f2dff9495f1b91ccfee98318e
Petr Walczysko <p.walczysko@dundee.ac.uk>
2017-07-02 17:27:23 +0100

    flake8 fix

--- components/tools/OmeroPy/src/omero/plugins/chown.py
+++ components/tools/OmeroPy/src/omero/plugins/chown.py
@@ -106,12 +106,15 @@ class ChownControl(GraphControl):
         self._pre_objects(parser)
         parser.add_argument(
             "obj", nargs="*", type=GraphArg(self.cmd_type()),
-            help="either objects to be processed in the form <Class>:<Id> or user(s) to transfer ownership of all data from in the form Experimenter:<Id>")
+            help="either objects to be processed in the form <Class>:<Id>"
+                 " or user(s) to transfer ownership of all data from in the"
+                 " form Experimenter:<Id>")
 
     def _pre_objects(self, parser):
         parser.add_argument(
             "usr", nargs="?", type=ExperimenterArg,
-            help="""user to transfer ownership of specified objects or all objects owned by specified user(s) to""")
+            help="user to transfer ownership of specified objects or all"
+                 " objects owned by specified user(s) to")
 
     def _process_request(self, req, args, client):
         # Retrieve user id
